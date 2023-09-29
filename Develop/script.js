37060589b0baa39a47c9f5a816e740e865b47c21
Rodrigo <rodrigo0599@hotmail.com>
2023-09-29 13:12:18 -0400

    1st

--- Develop/script.js
+++ Develop/script.js
@@ -3,7 +3,7 @@ var symbols = "!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
 var numbers = "0123456789";
 var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var lowercase = "abcdefghijklmnopqrstuvwxyz";
-
+// Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 var generatePassword = function () {
@@ -52,11 +52,11 @@ var generatePassword = function () {
   
   return password;
 }
-
+// Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
 }
-
+// Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
